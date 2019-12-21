475e94d0c432a1de8abf66d509d13ad3a6343e3c
Edicarla Sillva <edicarlasillva@gmail.com>
2019-12-21 16:51:57 -0300

    Intro React

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom"; // Integração do React com a árvore de elementos
+
+import App from "./App";
+console.log("teste");
+render(<App />, document.getElementById("app"));
+
+// const soma = (a, b) => a + b;
+
+// alert(soma(1, 10));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    // código transpilado pelo webpack
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // testar arquivos terminados em .js
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/, // testar aquivos que terminam com .css
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  }
+};
